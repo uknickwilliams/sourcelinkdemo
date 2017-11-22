35585f950dec6155adcb4c820b53f8b87fe54e18
nick.williams <nick@conveyornetworks.co.uk>
2017-11-22 09:30:47 +0000

    Initial commit

---
+++ SourceLinkStandard/Speaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SourceLinkStandard
+{
+    public class Speaker
+    {
+        private readonly Random _random = new Random();
+
+        public string MakeNoise()
+        {
+            if (_random.Next(0, 10) > 8)
+                return "Random";
+
+            return "Hello World";
+        }
+    }
+}
